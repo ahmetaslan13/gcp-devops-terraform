356b9e70d941438a7235a885d993f14465151dac
ahmetaslan13 <ahmetaslan.work@gmail.com>
2024-03-12 08:19:06 -0400

    feature

--- main.tf
+++ main.tf
@@ -8,18 +8,18 @@ data "google_project" "current" {}
 
 # Define a Google Storage Bucket resource using the project ID
 resource "google_storage_bucket" "my_bucket" {
-  name          = "${data.google_project.current.project_id}-bucket-tfstate"  # Using the project ID from provider configuration
-  location      = "US"                             # Location of the bucket
-  force_destroy = false                             # Allow Terraform to delete the bucket even if it contains objects
-  
+  name          = "${data.google_project.current.project_id}-bucket-tfstate" # Using the project ID from provider configuration
+  location      = "US"                                                       # Location of the bucket
+  force_destroy = false                                                      # Allow Terraform to delete the bucket even if it contains objects
+
   # Additional configuration options can be added here as needed
 }
 
 # Define a Google Storage Bucket resource with a random ID prefix
 resource "google_storage_bucket" "random_bucket" {
   name          = "${random_id.bucket_prefix.hex}-bucket"
-  location      = "US"                             # Location of the bucket
-  force_destroy = false                             # Allow Terraform to delete the bucket even if it contains objects
-  
+  location      = "US"  # Location of the bucket
+  force_destroy = false # Allow Terraform to delete the bucket even if it contains objects
+
   # Additional configuration options can be added here as needed
 }
